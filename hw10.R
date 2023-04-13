f01eea2a4cc13c9f19d5f0a6f24dc2867057b462
egriggs23 <122321958+egriggs23@users.noreply.github.com>
2023-04-12 20:57:25 -0400

    hw10

--- hw10.R
+++ hw10.R
@@ -35,17 +35,40 @@ print(trt_group)
 z <- c(runif(4) + 1, runif(5) + 10, runif(20)+4)
 print(z)
 
-data <- data.frame(trt=trt_group,res=z)
+df <- data.frame(trt=trt_group,res=z)
 print(data)
 
 #b
+shuffled_df<- data.frame(c1=df$trt, c2=sample(df$res))
+vector<-aggregate(shuffled_df$c2, list(shuffled_df$c1), FUN=mean)
+print(vector)
 
-  shuffle<- function(x){shuffled_data <-data.frame(c1=df$trt, c2=sample(df$res))
-  setDT(shuffled_data)
-  vector<-shuffled_data[ ,list(mean=mean(c2)), by=c1]
+  #c
+  #Use a for loop to repeat the function in b 100 times. Store the results in a data frame that has 1 column indicating the replicate number and 1 column for each new group mean, for a total of 4 columns.
+  
+shuffle <- function(data=NULL){
+  shuffled_df<- data.frame(c1=df$trt, c2=sample(df$res))
+  vector<-aggregate(shuffled_df$c2, list(shuffled_df$c1), FUN=mean) 
+}
+  
+  newdataframe <- expand.grid(replicate=1:100,
+                              Control=NA,
+                              TRT1=NA,
+                              TRT2=NA
+                              )
+  
+  for(i in 1:100) {
+    x<-shuffled_df<- data.frame(c1=df$trt, c2=sample(df$res))
+    vector<-aggregate(shuffled_df$c2, list(shuffled_df$c1), FUN=mean)
+    
+    newdataframe[i,1] <-i
+    newdataframe[i,2] <- x[1,2] 
+    newdataframe[i,3] <- x[2,2] 
+    newdataframe[i,4] <- x[3,2]
+}
+    
   }
-  print(vector)
-
+  
 
 
 
